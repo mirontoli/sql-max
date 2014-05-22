dddf76702cdeefb2aae14a22f3112d6c6def2a09
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-22 18:37:32 +0200

    Sets up grunt

---
+++ Gruntfile.js
@@ -0,0 +1,241 @@
+'use strict';
+var LIVERELOAD_PORT = 8605;
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // configurable paths
+  var localConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    local: localConfig,
+    watch: {
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          '<%= local.app %>/{,*/}*.html',
+          '{.tmp,<%= local.app %>}/styles/{,*/}*.css',
+          '{.tmp,<%= local.app %>}/scripts/{,*/}*.js',
+          '<%= local.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 8877,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, localConfig.app)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test')
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, localConfig.dist)
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= local.dist %>/*',
+            '!<%= local.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= local.app %>/scripts/{,*/}*.js'
+      ]
+    },
+    rev: {
+      dist: {
+        files: {
+          src: [
+            '<%= local.dist %>/scripts/{,*/}*.js',
+            '<%= local.dist %>/styles/{,*/}*.css',
+            '<%= local.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= local.dist %>/styles/fonts/*'
+          ]
+        }
+      }
+    },
+    useminPrepare: {
+      html: '<%= local.app %>/index.html',
+      options: {
+        dest: '<%= local.dist %>'
+      }
+    },
+    usemin: {
+      html: ['<%= local.dist %>/{,*/}*.html'],
+      css: ['<%= local.dist %>/styles/{,*/}*.css'],
+      options: {
+        dirs: ['<%= local.dist %>']
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= local.app %>/images',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: '<%= local.dist %>/images'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= local.app %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= local.dist %>/images'
+        }]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          /*removeCommentsFromCDATA: true,
+          // https://github.com/local/grunt-usemin/issues/44
+          //collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true*/
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= local.app %>',
+          src: ['*.html'],
+          dest: '<%= local.dist %>'
+        }]
+      }
+    },
+    // Put files not handled in other tasks here
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= local.app %>',
+          dest: '<%= local.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            'bower_components/**/*',
+            'images/{,*/}*.{gif,webp}',
+            'styles/fonts/*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= local.dist %>/images',
+          src: [
+            'generated/*'
+          ]
+        }]
+      }
+    },
+    concurrent: {
+      dist: [
+        'imagemin',
+        'svgmin',
+        'htmlmin'
+      ]
+    },
+    cdnify: {
+      dist: {
+        html: ['<%= local.dist %>/*.html']
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          '<%= local.dist %>/scripts/scripts.js': [
+            '<%= local.dist %>/scripts/scripts.js'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'connect:livereload',
+      'open',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'useminPrepare',
+    'concurrent:dist',
+    'concat',
+    'copy',
+    'cdnify',
+    'cssmin',
+    'uglify',
+    'rev',
+    'usemin'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    'build'
+  ]);
+};
