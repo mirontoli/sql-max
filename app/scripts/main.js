0e7f02b2cd95ef5623263775f10e37470000747a
Anatoly Mironov <mirontoli@gmail.com>
2014-05-24 00:28:23 +0200

    add info how to set max memory

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,10 @@
 		architecture: elements.architecture.value,
 		otherram: elements.otherram.value
 	};
+	function numberWithCommas(x) {
+		//http://stackoverflow.com/a/2901298/632117
+	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	}
 	var calculateMemory = function() {
 		//everything in MB
 		var oneGb = 1024; //MB
@@ -21,12 +25,12 @@
 		// "hard coded" options
 		var architecture = values.architecture;
 		var totalram = parseInt(values.totalram) * oneGb;
-		var corenumber = parseInt(values.cores);;
+		var corenumber = parseInt(values.cores);
 		var otherApps = parseInt(values.otherram);
 
 
 		//thread stack sizes in MB
-		var threadStackSizes = { "x86": 1, "x64": 2, "IA64": 4 };
+		var threadStackSizes = { 'x86': 1, 'x64': 2, 'IA64': 4 };
 
 		var threadStackSize = threadStackSizes[architecture];
 
@@ -37,17 +41,19 @@
 		var temp = corenumber / 4;
 
 		var maxMemory = (totalram - forOS - otherApps - (sqlThreads * threadStackSize) - (1024 * Math.ceil(temp)));
-		return maxMemory;
+		return Math.ceil(maxMemory);
 	};
 	var handleChangeEvent = function(e) {
 		var id = e.target.id;
 		values[id] = elements[id].value;
-		elements.result.innerText = calculateMemory();
+		elements.result.innerText = numberWithCommas(calculateMemory());
 	};
-	elements.architecture.addEventListener("change", handleChangeEvent);
-	elements.totalram.addEventListener("change", handleChangeEvent);
-	elements.cores.addEventListener("change", handleChangeEvent);
-	elements.otherram.addEventListener("change", handleChangeEvent);
+	elements.architecture.addEventListener('change', handleChangeEvent);
+	elements.totalram.addEventListener('change', handleChangeEvent);
+	elements.cores.addEventListener('change', handleChangeEvent);
+	elements.otherram.addEventListener('change', handleChangeEvent);
+
+	elements.result.innerText = numberWithCommas(calculateMemory());
 })();
 /*
 
