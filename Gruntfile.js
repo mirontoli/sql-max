e1970dd61bf3afcf8d2c7c32b4ab404d627b864a
Anatoly Mironov <mirontoli@gmail.com>
2014-05-23 06:52:21 +0200

    add CNAME back to the application

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,6 +171,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
+            'CNAME',
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
